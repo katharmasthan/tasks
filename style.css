4273408f1b9fc5b2d74374988dbf0c24bcc0ef30
katharmasthan <kadhar6069@gmail.com>
2025-12-10 18:57:39 +0530

    task8

---
+++ style.css
@@ -0,0 +1,19 @@
+.external-heading {
+    color: brown;
+    font-size: 26px;
+}
+
+.external-about {
+    color: darkcyan;
+    font-size: 20px;
+}
+
+.only-external {
+    color: teal;
+    font-size: 30px;
+}
+
+.only-external-para {
+    color: darkblue;
+    font-size: 18px;
+}
