e18fbbe9218f691946068b9f15704cf82dac8647
katharmasthan <kadhar6069@gmail.com>
2025-12-26 20:31:16 +0530

    Add files via upload

---
+++ task java 1.js
@@ -0,0 +1,52 @@
+let a1 = "10";
+let b1 = 5;
+let res1 = a1 + b1;
+console.log(res1);
+console.log(typeof res1);
+
+let x = 10;
+let y = "10";
+console.log(x == y);
+console.log(x === y);
+
+let num = 15;
+if (num > 0) console.log("Positive");
+else if (num < 0) console.log("Negative");
+else console.log("Zero");
+
+let p = 10;
+let q = 20;
+let r = "30";
+let sum = p + q + r;
+console.log(sum);
+console.log(typeof sum);
+
+let value = true;
+let res5 = Number(value) + 5;
+console.log(res5);
+console.log(typeof res5);
+
+let a = 7;
+let b = 3;
+console.log(a > b);
+
+let marks = "85";
+if (Number(marks) >= 40) console.log("Passed");
+else console.log("Failed");
+
+let age = 20;
+let hasID = false;
+console.log(age >= 18 && hasID);
+
+let count;
+console.log(count);
+console.log(typeof count);
+count = 10;
+console.log(count);
+console.log(typeof count);
+
+let num1 = "5";
+let num2 = "2";
+console.log(num1 + num2, typeof (num1 + num2));
+console.log(num1 - num2, typeof (num1 - num2));
+console.log(num1 * num2, typeof (num1 * num2));
